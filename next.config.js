0fa950145340a5e5178812df75359b3f1dc75bd7
Sourav <sourav@Souravs-MacBook-Air.local>
2023-11-16 02:34:23 +0600

    testimonial section done

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
    images: {
-      domains: ["randomuser.me"],
+      remotePatterns: [
+         {
+            protocol: "https",
+            hostname: "randomuser.me",
+            pathname: "**",
+         },
+      ],
    },
 }
 
